c9211e2e559c0aad332219e7260f332157cab985
Robert Chinem <robertchinem@MacBook-Air-de-Robert.local>
2021-09-17 20:08:53 -0300

    create interface Submissions

---
+++ src/models/Submission.ts
@@ -0,0 +1,11 @@
+import { Status } from './Status';
+
+export interface Submission {
+    source_code: string
+    language_id: number
+    stdin?: string
+    stderr?: string
+    stdout?: string
+    compile_output?: string
+    status?: Status
+}
