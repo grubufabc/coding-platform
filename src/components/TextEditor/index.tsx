a73f0ecdb8a1a02c184993b45e72bd4bb8bebff2
Robert Chinem <robertchinem@MacBook-Air-de-Robert.local>
2021-09-17 17:25:46 -0300

    change TextEditor

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,18 +1,40 @@
 import React from 'react'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/mode/clike/clike'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/python/python'
+import './style.css'
+import { Controlled as ControlledEditor } from 'react-codemirror2'
 
 
 interface TextEditorProps {
-
+    value: string
+    language: string
+    onChange: any
 }
 
+const TextEditor: React.FC<TextEditorProps> = ({ value, language, onChange }) => {
+    
+    function handleChange(value: string){
+        onChange(value)
+    }
 
-const TextEditor: React.FC<TextEditorProps> = () => {
     return (
-        <div>
-            <h1>Text editor</h1>
+        <div className="">
+            <ControlledEditor 
+                onBeforeChange={(_, __, value) => handleChange(value)}
+                value={value}
+                className="code-mirror-wrapper code-wrapper"
+                options={{
+                    lineWrapping: true,
+                    mode: language,
+                    theme: 'dracula',
+                    lineNumbers: true,
+                }}
+            />
         </div>
     )
 }
-
 
 export default TextEditor
