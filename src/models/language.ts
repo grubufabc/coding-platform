8f1649ecb0bd55696a1debccbb784619fda2a13f
Robert Chinem <robertchinem@MacBook-Air-de-Robert.local>
2021-09-19 00:04:32 -0300

    fix

---
+++ src/models/language.ts
@@ -0,0 +1,5 @@
+export interface Language {
+    name: string
+    id: number
+    mode: string
+}
---
+++ src/models/submission.ts
@@ -0,0 +1,11 @@
+import { Status } from './status'
+
+export interface Submission {
+    source_code: string
+    language_id: number
+    stdin?: string
+    stderr?: string
+    stdout?: string
+    compile_output?: string
+    status?: Status
+}
