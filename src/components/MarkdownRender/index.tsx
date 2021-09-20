31d3cc6a66a92273a25e6f57273f8904afd92b6b
Robert Chinem <robertchinem@MacBook-Air-de-Robert.local>
2021-09-20 15:13:37 -0300

    implentation of Markdown render

--- src/components/MarkdownRender/index.tsx
+++ src/components/MarkdownRender/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+import './style.css'
 
 interface MarkdownRenderProps {
     text: string
 }
 
 
-const MarkdownRender: React.FC<MarkdownRenderProps> = ({ text='' }) => {
+const MarkdownRender: React.FC<MarkdownRenderProps> = ({ text }) => {
     return (
         <div>
-            { text }
+            <article className="markdown-body">
+                <ReactMarkdown children={text} remarkPlugins={[remarkGfm]} />
+            </article>
         </div>
     )
 }
