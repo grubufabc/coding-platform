b4cd735058e87d58349e3d589c37bcd918ef913a
Robert Chinem <robertchinem@MacBook-Air-de-Robert.local>
2022-01-14 17:28:10 -0300

    fix landing page

--- src/pages/LandingPage/MainSection/index.tsx
+++ src/pages/LandingPage/MainSection/index.tsx
@@ -4,7 +4,7 @@ import CodeInspectionIcon from "../icons/CodeInspectionIcon"
 
 const MainSection: React.FC = () => {
     return (
-        <div className="landing-page-slide" style={{ height: 'calc(100vh - 3rem)' }}>
+        <div className="landing-page-slide" style={{ height: 'calc(100vh - 4rem)' }}>
             <div style={{ width: '50%', paddingBottom: '8rem' }}>
                 <h1 className="display-1 fw-bolder">Aprenda Praticando</h1>
                 <p className="fs-5 text-secondary my-4">Aprenda Ciência da Computação na prática. <br /> Uma plataforma completa de programação.</p>
