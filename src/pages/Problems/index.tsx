9770076293be673668639545bce839e7d7dc8113
Wesley <wesleypersilva@gmail.com>
2022-03-04 23:14:18 -0300

    Change text "Problems" to "Problemas"

--- src/pages/Problems/index.tsx
+++ src/pages/Problems/index.tsx
@@ -6,57 +6,57 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 
 const Problems: React.FC = () => {
-	const { request, loading } = useFetch();
-	const [problems, setProblems] = React.useState<Problem[]>();
+  const { request, loading } = useFetch();
+  const [problems, setProblems] = React.useState<Problem[]>();
 
-	React.useEffect(() => {
-		const listProblems = async () => {
-			const { url, options } = API_GET_PROBLEMS();
-			const { json } = await request(url, options);
-			setProblems(json as Problem[]);
-		};
-		if (problems === undefined) listProblems();
-	}, [problems, request]);
+  React.useEffect(() => {
+    const listProblems = async () => {
+      const { url, options } = API_GET_PROBLEMS();
+      const { json } = await request(url, options);
+      setProblems(json as Problem[]);
+    };
+    if (problems === undefined) listProblems();
+  }, [problems, request]);
 
-	return (
-		<React.Fragment>
-			<Header />
-			<div className="m-5">
-				<h1 className="mb-5">Problems</h1>
-				{loading && (
-					<div className="d-flex align-items-center">
-						<strong>Carregando problemas...</strong>
-						<div
-							className="spinner-border ms-5"
-							role="status"
-							aria-hidden="true"
-						></div>
-					</div>
-				)}
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="m-5">
+        <h1 className="mb-5">Problemas</h1>
+        {loading && (
+          <div className="d-flex align-items-center">
+            <strong>Carregando problemas...</strong>
+            <div
+              className="spinner-border ms-5"
+              role="status"
+              aria-hidden="true"
+            ></div>
+          </div>
+        )}
 
-				{problems && (
-					<table className="table table-striped">
-						<thead>
-							<tr>
-								<th scope="col">Status</th>
-								<th scope="col">Título</th>
-							</tr>
-						</thead>
-						<tbody>
-							{problems.map((problem, index) => (
-								<tr key={index}>
-									<td className="border">-</td>
-									<td className="border p-3">
-										<Link to={`${problem._id}`}>{problem.title}</Link>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				)}
-			</div>
-		</React.Fragment>
-	);
+        {problems && (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Status</th>
+                <th scope="col">Título</th>
+              </tr>
+            </thead>
+            <tbody>
+              {problems.map((problem, index) => (
+                <tr key={index}>
+                  <td className="border">-</td>
+                  <td className="border p-3">
+                    <Link to={`${problem._id}`}>{problem.title}</Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Problems;
